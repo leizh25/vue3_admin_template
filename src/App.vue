31c8f0d27f43ae5aef390fbe0c42b8ee78a6600d
leizh25 <leizhuzheng@qq.com>
2023-09-25 14:12:25 +0800

    feat: addH1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="box">
-    <h1>我是app根组件</h1>
+    <h1>我新增一个标题</h1>
   </div>
 </template>
 <script setup lang="ts">
 for (let i = 0; i < 100; i++) {
   console.log(i)
-  
 }
 </script>
 <style scoped lang="scss">
