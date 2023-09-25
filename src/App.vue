13c812b2df9254d630c5ff5c74b81a71a38ee116
leizh25 <leizhuzheng@qq.com>
2023-09-25 15:05:39 +0800

    feat: alias src=@

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,13 @@
     <el-button type="success" size="deault" :icon="Edit">编辑按钮</el-button>
     <el-button type="danger" size="large" :icon="Delete">删除按钮</el-button>
     <el-pagination layout="prev, pager, next, jumper" :total="1000" />
+
+    <Test></Test>
   </div>
 </template>
 <script setup lang="ts">
 //引入图标组件
 import { Plus, Edit, Delete } from '@element-plus/icons-vue'
+import Test from '@/components/Test.vue'
 </script>
 <style scoped lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 //引入element-plus插件与样式
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 //配置element-plus国际化
-//@ts-ignore
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-expect-error
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 //获取应用实例对象
 const app = createApp(App)
