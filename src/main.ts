20d3dad3761b1747a3136a94989cdd2a1915e135
leizh25 <leizhuzheng@qq.com>
2023-09-25 13:39:55 +0800

    项目中prettier格式化工具的配置

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp(App).mount("#app");
+createApp(App).mount('#app')
+
+const fun = () => {
+  console.log('我爱你祖国')
+}
+fun()
