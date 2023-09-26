3570b20238cdabffd9fc72d77d03b98a3c41afeb
leizh25 <leizhuzheng@qq.com>
2023-09-26 22:36:04 +0800

    API接口统一管理

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <template>
-  <div>
-    <h1>测试axios二次封装</h1>
-  </div>
+  <div>App根组件</div>
 </template>
 <script setup lang="ts">
-import request from '@/utils/request.ts'
 import { onMounted } from 'vue'
-
-//当组件挂载完毕测试发一个请求
+import { reqLogin } from './api/user'
 onMounted(() => {
-  request({
-    url: '/user/login',
-    method: 'post',
-    data: {
-      username: 'admin',
-      password: '111111',
-    },
-  }).then((res) => {
-    console.log(res)
-  })
+  reqLogin({ username: 'admin', password: '111111' })
 })
 </script>
 <style scoped></style>
