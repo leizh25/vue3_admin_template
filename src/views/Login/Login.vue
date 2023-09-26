1bf4a66f3aa00ad209a43c2366da5b68270829a5
leizh25 <leizhuzheng@qq.com>
2023-09-26 23:40:10 +0800

    登录路由静态的搭建

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,7 +1,58 @@
 <template>
-  <div>
-    <h1>我是一级路由-登录</h1>
+  <div class="login_container">
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login_form">
+          <h1>Hello</h1>
+          <h2>欢迎来到硅谷甄选</h2>
+          <el-form-item>
+            <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input :prefix-icon="Lock" type="password" show-password v-model="loginForm.password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="login_btn" type="primary" size="default">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
-<script setup lang="ts"></script>
-<style scoped></style>
+
+<script setup lang="ts">
+import { Lock, User } from '@element-plus/icons-vue'
+import { reactive } from 'vue';
+//收集账号与密码的数据
+let loginForm = reactive({username:"admin",password:"111111"})
+</script>
+
+<style scoped lang="scss">
+.login_container {
+  width: 100%;
+  height: 100vh;
+  background: url('@/assets/images/background.jpg') no-repeat;
+  background-size: cover;
+}
+.login_form {
+  width: 80%;
+  position: relative;
+  top: 30vh;
+  background: url('@/assets/images/login_form.png') no-repeat;
+  background-size: cover;
+  padding: 40px;
+  h1 {
+    color: #fff;
+    font-size: 40px;
+  }
+  h2 {
+    color: #fff;
+    font-size: 20px;
+    margin: 20px 0;
+  }
+  .login_btn {
+    width: 100%;
+  }
+}
+</style>
