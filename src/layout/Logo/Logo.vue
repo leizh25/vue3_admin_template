4be9efc32075ad6ac816e4010e200cf9e5b0cb8f
leizh25 <leizhuzheng@qq.com>
2023-10-03 19:44:08 +0800

    style:代码格式化

--- src/layout/Logo/Logo.vue
+++ src/layout/Logo/Logo.vue
@@ -6,7 +6,7 @@
 </template>
 <script setup lang="ts">
 //引入设置标题与logo的配置文件
-import setting from '@/setting';
+import setting from '@/setting'
 </script>
 <style scoped lang="scss">
 .logo {
@@ -16,11 +16,11 @@ import setting from '@/setting';
   display: flex;
   align-items: center;
   padding: 20px;
-  img{
+  img {
     width: 40px;
     height: 40px;
   }
-  p{
+  p {
     font-size: $base-logo-title-fontSize;
     margin-left: 10px;
   }
