f57328fbc08a527a893eb05660ed577c62481e68
leizh25 <leizhuzheng@qq.com>
2023-10-03 14:00:14 +0800

    用户仓库的数据TS类型的定义

--- src/api/user/type.ts
+++ src/api/user/type.ts
@@ -6,7 +6,8 @@ export interface loginForm {
 
 //登录接口返回的数据的类型
 interface dataType {
-  token: string
+  token?: string
+  message?: string
 }
 export interface loginResponseData {
   code: number
--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -3,29 +3,32 @@ import { defineStore } from 'pinia'
 //引入接口
 import { reqLogin } from '@/api/user'
 //引入数据类型
-import type { loginForm } from '@/api/user/type'
+import type { loginForm, loginResponseData } from '@/api/user/type'
+import { UserState } from './types/type'
+//引入操作本地存储的工具方法
+import { GET_TOKEN, SET_TOKEN } from '@/utils/token'
 //创建用户小仓库
 let useUserStore = defineStore('User', {
   //小仓库存储数据的地方
-  state: () => {
+  state: (): UserState => {
     return {
-      token: localStorage.getItem('TOKEN'), //用户的唯一标识token
+      token: GET_TOKEN(), //用户的唯一标识token
     }
   },
   //处理异步或者逻辑的地方
   actions: {
     //用户登录的方法
     async userLogin(data: loginForm) {
-      let res: any = await reqLogin(data)
+      let res: loginResponseData = await reqLogin(data)
       // console.log('res: ', res);
       //登录请求成功200 ->token
       //登录请求失败201 ->登录失败错误信息
       if (res.code == 200) {
         //pinia仓库存储token
         //由于pinia或者vuex存储数据其实就是利用js对象
-        this.token = res.data.token
+        this.token = res.data.token as string
         //本地存储持久化存储一份
-        localStorage.setItem('TOKEN', res.data.token)
+        SET_TOKEN(res.data.token as string)
         //能保证当前async函数返回一个成功的promise
         return 'ok'
       } else {
